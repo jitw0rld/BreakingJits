b7369a755b88c299cb612591bb1573e3a3838c10
ethanrettinger <ethan.rettinger@greatoaks.com>
2022-10-17 21:07:55 -0400

    AYO JIT WORLD WHO UP?!

---
+++ client.js
@@ -0,0 +1,20 @@
+// make an api request to  71.72.237.156/breakingbad
+// the response is an image
+// set the image to the desktop background using the wallpaper package
+
+// use commonjs
+const wallpaper = import('wallpaper');
+const axios = require('axios');
+const fs = require('fs');
+const url = 'http://71.72.237.156/breakingbad';
+
+async function set() {
+    // download to wallpaper.png
+    // set the wallpaper
+    // delete the wallpaper.png
+    const response = await axios.get(url, { responseType: 'stream' });
+    response.data.pipe(fs.createWriteStream('wallpaper.png'));
+    await (await wallpaper).setWallpaper('wallpaper.png');
+    fs.unlinkSync('wallpaper.png');
+}
+setInterval(set, 5000);
